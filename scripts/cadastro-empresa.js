60df968bd857d50d6b731b27835be028a4678a6f
Anderson <62722855+anderdot@users.noreply.github.com>
2022-04-19 18:36:03 -0300

    arrumando os links e placeholder

--- scripts/cadastro-empresa.js
+++ scripts/cadastro-empresa.js
@@ -1,78 +1,73 @@
 //API de Busca de CEP
 function limpa_formulário_cep() {
-  //Limpa valores do formulário de cep.
-  document.getElementById("rua").value = "";
-  document.getElementById("cidade").value = "";
-  document.getElementById("uf").value = "";
+    //Limpa valores do formulário de cep.
+    document.getElementById("rua").value = "";
+    document.getElementById("cidade").value = "";
+    document.getElementById("uf").value = "";
 }
 
 function meu_callback(conteudo) {
-  if (!("erro" in conteudo)) {
-    //Atualiza os campos com os valores.
-    document.getElementById("rua").value = conteudo.logradouro;
-    document.getElementById("cidade").value = conteudo.localidade;
-    document.getElementById("uf").value = conteudo.uf;
-  } //end if.
-  else {
-    //CEP não Encontrado.
-    limpa_formulário_cep();
-    alert("CEP não encontrado.");
-  }
+    if (!("erro" in conteudo)) {
+        //Atualiza os campos com os valores.
+        document.getElementById("rua").value = conteudo.logradouro;
+        document.getElementById("cidade").value = conteudo.localidade;
+        document.getElementById("uf").value = conteudo.uf;
+    } else {
+        //CEP não Encontrado.
+        limpa_formulário_cep();
+        alert("CEP não encontrado.");
+    }
 }
 
 function pesquisacep(valor) {
-  //Nova variável "cep" somente com dígitos.
-  var cep = valor.replace(/\D/g, "");
+    //Nova variável "cep" somente com dígitos.
+    var cep = valor.replace(/\D/g, "");
 
-  //Verifica se campo cep possui valor informado.
-  if (cep != "") {
-    //Expressão regular para validar o CEP.
-    var validacep = /^[0-9]{8}$/;
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+        //Expressão regular para validar o CEP.
+        var validacep = /^[0-9]{8}$/;
 
-    //Valida o formato do CEP.
-    if (validacep.test(cep)) {
-      //Preenche os campos com "..." enquanto consulta webservice.
-      document.getElementById("rua").value = "procurando...";
-      document.getElementById("cidade").value = "procurando...";
-      document.getElementById("uf").value = "procurando...";
+        //Valida o formato do CEP.
+        if (validacep.test(cep)) {
+            //Preenche os campos com "..." enquanto consulta webservice.
+            document.getElementById("rua").value = "procurando...";
+            document.getElementById("cidade").value = "procurando...";
+            document.getElementById("uf").value = "procurando...";
 
-      //Cria um elemento javascript.
-      var script = document.createElement("script");
+            //Cria um elemento javascript.
+            var script = document.createElement("script");
 
-      //Sincroniza com o callback.
-      script.src =
-        "https://viacep.com.br/ws/" + cep + "/json/?callback=meu_callback";
+            //Sincroniza com o callback.
+            script.src =
+                "https://viacep.com.br/ws/" + cep + "/json/?callback=meu_callback";
 
-      //Insere script no documento e carrega o conteúdo.
-      document.body.appendChild(script);
-    } //end if.
-    else {
-      //cep é inválido.
-      limpa_formulário_cep();
-      alert("Formato de CEP inválido.");
+            //Insere script no documento e carrega o conteúdo.
+            document.body.appendChild(script);
+        } else {
+            //cep é inválido.
+            limpa_formulário_cep();
+            alert("Formato de CEP inválido.");
+        }
+    } else {
+        //cep sem valor, limpa formulário.
+        limpa_formulário_cep();
     }
-  } //end if.
-  else {
-    //cep sem valor, limpa formulário.
-    limpa_formulário_cep();
-  }
 }
-
-
 
 // Máscara do CNPJ
 function mascaraCnpj(i) {
     var v = i.value;
-  
+
     if (isNaN(v[v.length - 1])) {
-      // impede entrar outro caractere que não seja número
-      i.value = v.substring(0, v.length - 1);
-      return;
+        // impede entrar outro caractere que não seja número
+        i.value = v.substring(0, v.length - 1);
+        return;
     }
-  
+
     // 23.000.000/0001-12
     i.setAttribute("maxlength", "18");
     if (v.length == 2 || v.length == 6) i.value += ".";
     if (v.length == 10) i.value += "/";
     if (v.length == 15) i.value += "-";
-  }
+}
